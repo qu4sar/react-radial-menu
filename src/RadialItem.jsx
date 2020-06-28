dca3e97dc679e5ad9bed423c445422e900521a7e
Salif Omar Faye <salifomar.faye@stride-it.com>
2020-06-28 21:51:32 +0200

    fix

--- src/RadialItem.jsx
+++ src/RadialItem.jsx
@@ -1,64 +1,69 @@
-import React, { Component, Fragment } from "react"
-import PropTypes from "prop-types"
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class RadialItem extends Component {
-	handleClick = event => {
-		event.preventDefault();
-	}
+  handleClick = (event) => {
+    event.preventDefault();
+  };
 
-    render () {
-        const {
-            className,
-            size,
-            top,
-            left,
-            image,
-            handleClick,
-            children
-        } = this.props
-        const marginValue = (-1) * (size / 2) + "px";
+  render() {
+    const {
+      className,
+      size,
+      top,
+      left,
+      image,
+      handleClick,
+      children,
+      href,
+    } = this.props;
+    const marginValue = -1 * (size / 2) + "px";
 
-      	const itemStyle = {
-            top,
-            left,
-            width: `${size}px`,
-            height: `${size}px`,
-            lineHeight: `${size}px`,
-            backgroundImage: image,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover"
-      	};
+    const itemStyle = {
+      top,
+      left,
+      width: `${size}px`,
+      height: `${size}px`,
+      lineHeight: `${size}px`,
+      backgroundImage: image,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+    };
 
-        if (image !== "none") {
-            itemStyle.border = "none";
-            itemStyle.boxShadow = "none";
+    if (image !== "none") {
+      itemStyle.border = "none";
+      itemStyle.boxShadow = "none";
 
-            if (className !== "radial-menu-button") {
-                itemStyle.backgroundColor = "transparent";
-            }
-        }
-
-        return <a
-            className={className}
-            style={itemStyle}
-            onClick={handleClick}
-        >
-            {children}
-        </a>
+      if (className !== "radial-menu-button") {
+        itemStyle.backgroundColor = "transparent";
+      }
     }
+
+    return (
+      <Link
+        className={className}
+        style={itemStyle}
+        onClick={handleClick}
+        to={href}
+      >
+        {children}
+      </Link>
+    );
+  }
 }
 
 RadialItem.propTypes = {
-    className: PropTypes.string,
-    top: PropTypes.string,
-    left: PropTypes.string,
-    value: PropTypes.string,
-    size: PropTypes.number
-}
+  className: PropTypes.string,
+  top: PropTypes.string,
+  left: PropTypes.string,
+  value: PropTypes.string,
+  size: PropTypes.number,
+};
 
 RadialItem.defaultProps = {
-    image: "none",
-    size: 100
-}
+  image: "none",
+  size: 100,
+};
 
-export default RadialItem
+export default RadialItem;
